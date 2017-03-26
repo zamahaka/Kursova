d58ec5c442db4e7ce3b5c76446044f952061d65d
Yura Stetsyk <yustetsic@gmail.com>
2017-03-26 18:21:45 +0300

    Initial commit

---
+++ app/src/main/java/com/nulp/zamahaka/kursova/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.nulp.zamahaka.kursova
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initViews()
+    }
+
+    private fun initViews() {
+
+    }
+}
